e8849562897947e7cee3621ffe34c50fc37894e7
JasonDengPup <puppydengnus@gmail.com>
2026-08-21 10:25:28 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+const assetPrefix = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 export const metadata: Metadata = {
   title: "中国工商银行外部数据产品管理平台",
   description: "商业银行外部数据产品标准化建设管理概念原型",
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
+    icon: `${assetPrefix}/favicon.svg`,
+    shortcut: `${assetPrefix}/favicon.svg`,
   },
 };
 
